e127d27b0cfb7f407ad9676a9209261ac6840f29
medaparedd <medapareddysudhasri@gmail.com>
2024-01-01 16:21:21 +0530

    this is new one

--- program.sh
+++ program.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
 echo "enter your name:"
 read name
 age=$1
@@ -9,7 +15,7 @@ echo "age is: $1"
 echo "profession is: $2"
 if [ $name == sudha ]
 then
-    echo "give access"
+    echo "$G give access $N"
 else
-    echo "reject"
+    echo "$R reject $N"
 fi
