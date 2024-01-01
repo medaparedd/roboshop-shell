23a9fbb275a1506149b47593e56d12f3c9aee021
medaparedd <medapareddysudhasri@gmail.com>
2024-01-01 22:40:37 +0530

    this is new one

--- idea.sh
+++ idea.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 num1=$1
 num2=$2
-VALIDATE(){
+
     if [$num1==0 || $num2==0 ]
     then 
         echo "not possible to proceed"
@@ -9,11 +9,4 @@ VALIDATE(){
     else
         echo "proceed for further process"
     fi
-}
-
-yum install mysql -y
-VALIDATE $? "my number is coming"
-
-yum install git -y
-VALIDATE $? "my number is coming fast"
 
